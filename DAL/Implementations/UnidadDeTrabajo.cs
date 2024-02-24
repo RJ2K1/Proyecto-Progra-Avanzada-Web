b7691372c3c41135e4741f341dd57e6592b1d2dc
christ48 <christopheralvarez220302@gmail.com>
2024-02-24 10:15:16 -0600

    Cambios hechos para unidad de trabajo

---
+++ DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo : IUnidadDeTrabajo
+    {
+
+      
+
+        public IUsuariosDAL _usuariosDAL { get; }
+        public ITicketsDAL _ticketsDAL { get; }
+
+    private readonly ProyectoWebContext _context;
+
+    public UnidadDeTrabajo(ProyectoWebContext proyectoWebContext,
+                            IUsuariosDAL usuariosDAL,
+                            ITicketsDAL ticketsDAL)
+    {
+        _context = proyectoWebContext;
+        _usuariosDAL = usuariosDAL;
+        _ticketsDAL = ticketsDAL;
+
+    }
+
+
+    public bool Complete()
+    {
+        try
+        {
+            _context.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+
+            return false;
+        }
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
+}
+
---
+++ DAL/Interfaces/IUnidadDeTrabajo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnidadDeTrabajo: IDisposable
+    {
+        public interface IUnidadDeTrabajo : IDisposable
+        {
+            IUsuariosDAL _usuariosDAL { get; }
+            ITicketsDAL _ticketsDAL { get; }
+
+            bool Complete();
+        }
+    }
+}
