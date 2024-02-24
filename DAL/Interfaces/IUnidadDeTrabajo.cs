3a9ac8e3051130a17437f82562f74f235ffb657a
RJ2K1 <rzamora50148@ufide.ac.cr>
2024-02-24 10:36:32 -0600

    Creacion de Unidad de trabajo y carpetas
    Creacion de carpetas Models, Services, Implementations e interfaces en el backend

---
+++ DAL/Interfaces/IUnidadDeTrabajo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DAL.Interfaces
+{
+    public interface IUnidadDeTrabajo : IDisposable
+    {
+        IUsuariosDAL UsuariosDAL { get; }
+        bool Complete();
+    }
+}
